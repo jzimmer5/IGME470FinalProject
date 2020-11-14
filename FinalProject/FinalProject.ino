07a00fd67ae58670a9d07c337c6a8dd3714259d7
jzimmer5 <jrz4547@rit.edu>
2020-11-14 15:32:25 -0500

    Created the outline

---
+++ FinalProject/FinalProject.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void thinkspeakSubmit() {
+  
+}
+
+void potentionmeterRGB() {
+  
+}
+
+void buttonBuzzer() {
+  
+}
+
+void distanceUltrasonic() {
+  
+}
+
+void switchLED() {
+  
+}
