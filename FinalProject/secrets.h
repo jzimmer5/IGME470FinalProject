fcade1f0b210344b900d4f87d4f3777acb55856f
jzimmer5 <42787474+jzimmer5@users.noreply.github.com>
2020-11-23 16:23:41 -0500

    Update secrets.h

--- FinalProject/secrets.h
+++ FinalProject/secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "MySpectrumWiFi-2G"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "littlebubble378"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID_COUNTER 298725 			
 #define SECRET_READ_APIKEY_COUNTER "UD089VRECSZYFIZV"	//API Key for Test 
