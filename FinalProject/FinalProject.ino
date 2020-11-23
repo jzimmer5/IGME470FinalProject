602c660e54ec977ee45b26ed88b3d4413010bec0
sl4956 <42871316+sl4956@users.noreply.github.com>
2020-11-23 15:42:51 -0500

    Finalized the task methods
    I tested each of the tasks individually, they should all work.

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -1,3 +1,4 @@
+
 #include "ThingSpeak.h"
 #include <WiFiNINA.h>
 #include "secrets.h"
@@ -12,35 +13,59 @@ char pass[] = SECRET_PASS;   // your network password
 WiFiClient  client;
 
 // channel details
+
 unsigned long channelNumber = SECRET_CH_ID_COUNTER;
 const char * myAPIKey = SECRET_READ_APIKEY_COUNTER;
-unsigned int player1FieldNumber = SECRET_PLAYER_ONE_FIELD_NUMBER; 
+unsigned int player1FieldNumber = SECRET_PLAYER_ONE_FIELD_NUMBER;
 unsigned int player2FieldNumber = SECRET_PLAYER_TWO_FIELD_NUMBER;
 
-//set this to where your buzzer is connected
-const int buzzer = 9;
 
-//set this to where your button is connected
-const int button = 3;
+int turnLight = 1;
 
-//set this to where your slide switch is connected
-const int slideSwitch = 12;
+//the pins for the RGB LED task
+int rgbLEDPinR=3;
+int rgbLEDPinG=5;
+int rgbLEDPinB=6;
+int rgbAnalog=A0;
 
-//set this to where your led switch is connected
-const int led = 4;
+//the pins for the buzzer task
+int buttonPin = 7;
+int buzzerPin = 8;
 
-//set this to true if you are going to be player 1
+//the pins for the distance sensor task
+int triggerPin = 9;
+int echoPin = 10;
+
+//the pins for the switch task
+int switchLED = 11;
+int switchPin = 12;
+
+bool taskAssigned = false;
+int currentTask;
+int taskParameter;
+int previousSwitchState;
+bool taskComplete = false;
 bool start = true;
+
+unsigned long millisCounter=0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  //set up all the pins we'll be using
+  pinMode(rgbLEDpinR,OUTPUT);
+  pinMode(rgbLEDpinG,OUTPUT);
+  pinMode(rgbLEDpinB,OUTPUT);
+  pinMode(rgbAnalog,INPUT);
+  pinMode(buttonPin,INPUT);
+  pinMode(buzzerPin,OUTPUT);
+  pinMode(triggerPin,OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(switchLED,OUTPUT);
+  pinMode(switchPin,INPUT_PULLUP);
+  pinMode(turnLight,OUTPUT);
 
-  pinMode(buzzer,OUTPUT);
-  pinMode(led,OUTPUT);
-  pinMode(button,INPUT);
-  pinMode(slideSwitch,INPUT);
-
+  previousSwitchState = digitalRead(switchPin);
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
@@ -67,12 +92,56 @@ void loop() {
   
   //all task method calls should be called within this if statement
   if(start) {
-    //switch goes here
-  
-    calculateTime();
+    //designate a task for this turn
+    if(!taskAssigned){
+      currentTask=getNewTask();
+      taskAssigned = true;
+    }
+    
+    while(!taskComplete){
+      //turn on the light that tells you it's your turn
+      digitalWrite(turnLight, HIGH);
+      
+      //run the designated task method for the assigned task
+      switch(currentTask)
+      {
+        case 0:
+          potentiometerRGB(taskParameter);
+          break;
+        case 1:
+          buttonBuzzer();
+          break;
+        case 2:
+          distanceUltrasonic(taskParameter);
+          break;
+        case 3:
+          switchLED();
+          break;
+        
+      }
+      calculateTime();
+    }
+  }
+  else
+  {
+    digitalWrite(turnLight, HIGH);
   }
 
-  start = checkTurn();
+   start = checkTurn();
+}
+
+int getNewTask()
+{
+  tastComplete=false;
+  taskParameter = random(0,1024);
+  return random(0,4);
+}
+
+void completeTask()
+{
+  taskComplete=true;
+  taskAssigned = false;
+  millisCounter = 0;
 }
 
 //checks to see if there has been a change in the opposite channel, if so then it is this arduino's turn
@@ -108,27 +177,125 @@ void uploadTime() {
   }
 }
 
-void potentionmeterRGB() {
+void potentiometerRGB(int target) {
   
+  int input = analogRead(rgbAnalog);
+  Serial.print(input);
+  Serial.print("\t");
+  Serial.println(target);
+  //check if the potentiometer is set to a value close enough to the target
+  if(abs(input-target)<3)
+  {
+      millisCounter++;
+      if(millisCounter%50==0)
+      {
+        if(blinkVal==0)
+        {
+          blinkVal=255;
+        }
+        else
+        {
+          blinkVal=0;
+        }
+      }
+      if(millisCounter>=500)
+      {
+        completeTask();
+      }
+      analogWrite(rgbLEDPinR,blinkVal);
+      analogWrite(rgbLEDPinG,blinkVal);
+      analogWrite(rgbLEDPinB,blinkVal);
+  }
+  else{
+    millisCounter = 0;
+    
+    double  r;
+    double g;
+    double b;
+    int circ = map(input,0,1023,0,359);
+    int deg = (int)floor(circ/60.0);
+    switch(deg)
+    {
+      case 0:
+        r=1;
+        g=(circ-(60*deg))/60.0;
+        b=0;
+        break;
+      case 1:
+        r=1-(circ-60*deg)/60.0;
+        g=1;
+        b=0;
+        break;
+      case 2:
+        r=0;
+        g=1;
+        b=(circ-(60*deg))/60.0;
+        break;
+      case 3:
+        r=0;
+        g=1-(circ-60*deg)/60.0;
+        b=1;
+        break;
+      case 4:
+        r=(circ-(60*deg))/60.0;
+        g=0;
+        b=1;
+        break;
+      case 5:
+        r=1;
+        g=0;
+        b=1-(circ-60*deg)/60.0;
+        break;  
+    }
+    
+    analogWrite(rgbLEDPinR,(1.0-r)*255);
+    analogWrite(rgbLEDPinG,(1.0-g)*255);
+    analogWrite(rgbLEDPinB,(1.0-b)*255);
+  
+    delay(10);
+  }
 }
 
-void buttonBuzzer() {
+void buttonBuzzer()
+{
   tone(buzzer,1000);
-  if( digitalRead(button) == HIGH) {
+  if( digitalRead(button) == HIGH)
+  {
     noTone(buzzer);
     completeTask();
   }
 }
 
-void distanceUltrasonic() {
+void distanceUltrasonic(int target)
+{
+  float mappedTarget = map(target, 0,1023, 5,10);
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+
+  float timeDiff = pulseIn(echoPin, HIGH);
+  float distance = (timeDiff/2)/73.94;
+  if(abs(mappedTarget-distance)<1)
+  {
+    millisCounter+=1;
+    if(millisCounter>=2)
+    {
+      completeTask();
+    }
+  }
+  delay(250);
   
 }
 
-void switchLED() {
-   int slideState = digitalRead(slideSwitch);
-   digitalWrite(led, HIGH);
-   if (digitalRead(slideSwitch) != slideState) {
-      digitalWrite(led, LOW);
+void switchLED()
+{
+   int slideState = digitalRead(switchPin);
+   digitalWrite(switchLEDPin, slideState);
+   if (digitalRead(switchPin) != previousSwitchState)
+   {
       completeTask();
    }
+   previousSwitchState=slideState;
 }
