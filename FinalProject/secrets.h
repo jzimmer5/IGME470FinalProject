8820593017b5b6abf80011ee9f4780e781b2639d
sl4956 <42871316+sl4956@users.noreply.github.com>
2020-11-23 19:44:07 -0500

    Update secrets.h

--- FinalProject/secrets.h
+++ FinalProject/secrets.h
@@ -1,11 +1,13 @@
-// Use this file to store all of the private credentials 
-// and connection details
+#define SECRET_SSID ""		// replace MySSID with your WiFi network name
+#define SECRET_PASS ""	// replace MyPassword with your WiFi password
 
-#define SECRET_SSID "MySpectrumWiFi86-2G"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "littlebubble378"	// replace MyPassword with your WiFi password
+//#define SECRET_CH_ID_COUNTER 298725 			
+//#define SECRET_READ_APIKEY_COUNTER "TY9BO2J528DY7RL9"	//API Key for Test 
+//#define SECRET_WRITE_APIKEY_COUNTER "UD089VRECSZYFIZV" //write api key
 
-#define SECRET_CH_ID_COUNTER 298725 			
-#define SECRET_READ_APIKEY_COUNTER "TY9BO2J528DY7RL9"	//API Key for Test 
-#define SECRET_WRITE_APIKEY_COUNTER "UD089VRECSZYFIZV" //write api key
-#define SECRET_PLAYER_ONE_FIELD_NUMBER 2      //your field number
-#define SECRET_PLAYER_TWO_FIELD_NUMBER 1      //other persons field number
+
+#define SECRET_CH_ID_COUNTER 1240532       
+#define SECRET_READ_APIKEY_COUNTER "DNJ25U4LPFFV5OA5" //API Key for Test 
+#define SECRET_WRITE_APIKEY_COUNTER "3NCR3POK8ZCXL8SO" //write api key
+#define SECRET_PLAYER_ONE_FIELD_NUMBER 1      //your field number
+#define SECRET_PLAYER_TWO_FIELD_NUMBER 2      //other persons field number
